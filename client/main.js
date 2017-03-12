c63f973c835a4fdf6339c9d8e57eed38b89987c1
Christian <c.stamariab@gmail.com>
2017-03-12 16:41:55 -0300

    Import de account-ui desde Meteor y desact. insecure

--- client/main.js
+++ client/main.js
@@ -4,7 +4,7 @@ import ReacDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 //Import Components
 import CommentList from '../imports/client/containers/commentList'
-
+import '../imports/startup/accounts-ui'
 Meteor.startup(() => {
 	ReacDOM.render(<CommentList /> , document.getElementById('container'))
 })
--- imports/client/containers/commentList.js
+++ imports/client/containers/commentList.js
@@ -1,33 +1,59 @@
 import React , { PropTypes } from 'react'
-
+import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 
 import { createContainer } from 'meteor/react-meteor-data'
 
 import Comment from '../components/comment'
+import AccountsUiWrapper from '../components/accountsUiWrapper'
+
 import { CommentsCollection } from '../../collections/comments'
 
 class CommentList extends React.Component {
-	constructor(props) {
-	  super(props);
-	}
+
+  handleSubmit(e){
+    e.preventDefault()
+    let comment = ReactDOM.findDOMNode(this.refs.textInput).value
+
+    CommentsCollection.insert({
+      comment
+    })        
+  }
 	render() {
-		
+
 		let commentList = this.props.comments.map((comment) => {
 			return(<Comment key={comment._id} comment={comment.comment} />)
 		});
 		return (
-			<div>
-				{ commentList }
+			<div>       
+         <AccountsUiWrapper /> 
+         {
+         
+         
+             this.props.currentUser ?
+              <div>
+                 <form onSubmit={this.handleSubmit.bind(this)} action="">
+                   <input ref='textInput' type="text"/>
+                   <input type="submit" value="Guardar" />
+                 </form>
+                 { commentList }
+              </div>
+           : '' 
+         }  
+			   
+         
+				
 			</div>
 		)
 	}
 }
 CommentList.propTypes = {
-  comments: PropTypes.array.isRequired
+  comments: PropTypes.array.isRequired,
+  currentUser: PropTypes.object
 }
 export default createContainer(()=>{
 	return {
 		comments: CommentsCollection.find({}).fetch(), //fetch suscribe a la lista
+    currentUser: Meteor.user()
 	}
 }, CommentList) 
